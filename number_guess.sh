cf34863e00f684f03466cd531a58e0c3d3bb8bbb
cafce25 <cafce25@gmail.com>
2022-04-19 15:12:14 +0000

    feat: nice heading

--- number_guess.sh
+++ number_guess.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 PSQL="psql --username=freecodecamp --dbname=number_guess -t --no-align -c"
 
+echo -e "\n~~~~~ Number Guess ~~~~~\n"
 echo "Enter your username:"
 read USERNAME
 USER_ID="$($PSQL "SELECT user_id FROM users WHERE name = '$USERNAME'")"
