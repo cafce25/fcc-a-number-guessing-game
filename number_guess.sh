e4511c7bbf60fbee0a7ad4768040a65ed4a7b2a7
cafce25 <cafce25@gmail.com>
2022-04-19 15:19:35 +0000

    refactor: better variable names

--- number_guess.sh
+++ number_guess.sh
@@ -11,26 +11,27 @@ if [[ -z $USER_ID ]]; then
   USER_ID="$($PSQL "SELECT user_id FROM users WHERE name = '$USERNAME'")"
 else
   
-  $PSQL "SELECT COUNT(*), MIN(guesses) FROM games WHERE user_id = $USER_ID" | while IFS="|" read N MIN; do
-    echo "Welcome back, $USERNAME! You have played $N games, and your best game took $MIN guesses."
+  $PSQL "SELECT COUNT(*), MIN(guesses) FROM games WHERE user_id = $USER_ID" | while IFS="|" read NUMBER_OF_GAMES BEST_GAME_GUESSES; do
+    echo "Welcome back, $USERNAME! You have played $NUMBER_OF_GAMES games, and your best game took $BEST_GAME_GUESSES guesses."
   done
 fi
-NUMBER=$(( RANDOM % 1000 + 1 ))
+
+SECRET_NUMBER=$(( RANDOM % 1000 + 1 ))
 
 echo "Guess the secret number between 1 and 1000:"
-(( I = 1 ))
+(( NUMBER_OF_GUESSES = 1 ))
 while :; do
   read GUESS
   if [[ ! $GUESS =~ ^[0-9]+$ ]]; then
     echo "That is not an integer, guess again:"
-  elif (( GUESS > NUMBER )); then
+  elif (( GUESS > SECRET_NUMBER )); then
     echo "It's lower than that, guess again:"
-  elif (( GUESS < NUMBER )); then
+  elif (( GUESS < SECRET_NUMBER )); then
    echo "It's higher than that, guess again:"
   else
-    echo "You guessed it in $I tries. The secret number was $NUMBER. Nice job!"
+    echo "You guessed it in $NUMBER_OF_GUESSES tries. The secret number was $SECRET_NUMBER. Nice job!"
     break
   fi
-  (( I += 1 ))
+  (( NUMBER_OF_GUESSES += 1 ))
 done
-INSERT_GAME_RESULT=$($PSQL "INSERT INTO games(user_id, guesses) VALUES($USER_ID, $I)")
+INSERT_GAME_RESULT=$($PSQL "INSERT INTO games(user_id, guesses) VALUES($USER_ID, $NUMBER_OF_GUESSES)")
